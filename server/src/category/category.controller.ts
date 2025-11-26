cf22ea725cc28e32533e1a3888228e53b8a750f5
NarsheS <shadownightmaresnw@gmail.com>
2025-11-26 19:11:13 -0300

    minor changes on categories

--- server/src/category/category.controller.ts
+++ server/src/category/category.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Post, Body, Get, Delete, ParseIntPipe, Param } from '@nestjs/common';
 import { CategoryService } from './category.service';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { Roles } from 'src/common/roles/roles.decorator';
+import { Role } from 'src/user/user.entity';
 
 @Controller('categories')
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
 
   @Post()
+  @Roles(Role.ADMIN)
   create(@Body() dto: CreateCategoryDto) {
     return this.categoryService.createCategory(dto);
   }
@@ -17,6 +20,7 @@ export class CategoryController {
   }
 
   @Delete(':id')
+  @Roles(Role.ADMIN)
   remove(@Param('id', ParseIntPipe) id: number){
     return this.categoryService.deleteCategory(id);
   }
