05b3a801e37074a05eccb3f6edeb170a3d39c86e
NarsheS <shadownightmaresnw@gmail.com>
2025-12-19 20:54:27 -0300

    fix render

--- server/src/payment/payment.service.ts
+++ server/src/payment/payment.service.ts
@@ -29,7 +29,7 @@ export class PaymentService implements OnModuleInit {
     }
 
     this.stripe = new Stripe(secretKey, {
-      apiVersion: '2025-11-17.clover',
+      apiVersion: '2025-12-15.clover',
     });
   }
 
