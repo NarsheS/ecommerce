b0e2bd8efec5f57b4ad35493c54feb90023c741d
NarsheS <shadownightmaresnw@gmail.com>
2026-01-08 16:22:13 -0300

    realocated homepage

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "./context/AuthContext";
-import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { AppSidebar } from "@/components/app-sidebar";
 import { Toaster } from "@/components/ui/sonner";
 
 const geistSans = Geist({
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,56 @@
-import HomePage from "./Pages/HomePage";
+"use client"
 
+import { Button } from "@/components/ui/button"
+import { useAuth } from "@/app/context/AuthContext"
+import { useRouter } from "next/navigation"
+import { Navbar04 } from "@/components/ui/shadcn-io/navbar-04"
+import UserMenu from "@/components/UserMenu"
+import { api, setAuthToken } from "./services/api"
 
-export default function Home() {
+const Home = () => {
+  const { accessToken, refresh, setAccessToken, user } = useAuth();
+  const router = useRouter();
+
+  const isAuthenticated = !!accessToken
+
+  const handleLoginClick = async () => {
+    const ok = await refresh()
+    if (!ok) router.push("/login");
+  }
+
+  const handleLogout = async () => {
+    try {
+      await api.post("/auth/logout");
+    } catch {}
+    finally {
+      setAccessToken(null);
+      setAuthToken(null);
+      router.replace("/login");
+    }
+  }
+
   return (
-    <HomePage />
-  );
+    <div className="flex flex-col gap-4">
+      <Navbar04
+        cartText="Carrinho"
+        searchPlaceholder="Buscar..."
+        rightSlot={
+          isAuthenticated ? (
+            <UserMenu
+              role={user?.role}
+              onLogout={handleLogout}
+            />
+          ) : (
+            <Button onClick={handleLoginClick}>
+              Entrar
+            </Button>
+          )
+        }
+      />
+
+      
+    </div>
+  )
 }
+
+export default Home
