a919b73ece4896c6db954f18745be1706003bace
NarsheS <shadownightmaresnw@gmail.com>
2026-02-03 16:57:47 -0300

    pricing added to cart module

--- server/src/cart/cart.module.ts
+++ server/src/cart/cart.module.ts
@@ -6,9 +6,10 @@ import { Products } from "../products/products.entity";
 import { CartService } from "./cart.service";
 import { CartController } from "./cart.controller";
 import { User } from "../user/user.entity";
+import { PricingService } from "src/products/pricing/pricing.service";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([Cart, CartItem, Products, User])],
+    imports: [TypeOrmModule.forFeature([Cart, CartItem, Products, User]), PricingService],
     providers: [CartService],
     controllers: [CartController],
     exports: [CartService],
