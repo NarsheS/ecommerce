449b5703fee117798dc31a50b5edb979e7dae02b
NarsheS <shadownightmaresnw@gmail.com>
2026-03-05 23:11:47 -0300

    minor bugfix on order.module.ts

--- server/src/order/order.module.ts
+++ server/src/order/order.module.ts
@@ -7,10 +7,11 @@ import { Cart } from '../cart/cart.entity';
 import { CartItem } from '../cart/cart-item.entity';
 import { OrderController } from './order.controller';
 import { PricingModule } from '../products/pricing/pricing.module';
+import { Address } from '../address/address.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Order, OrderItem, Cart, CartItem]),
+    TypeOrmModule.forFeature([Order, OrderItem, Cart, CartItem, Address]),
     PricingModule
   ],
   providers: [OrderService],
