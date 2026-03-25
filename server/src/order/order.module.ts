0b427656afcf148b0cebcd0370edcd1372fb35ec
NarsheS <shadownightmaresnw@gmail.com>
2026-03-25 16:33:01 -0300

    module fix

--- server/src/order/order.module.ts
+++ server/src/order/order.module.ts
@@ -9,10 +9,11 @@ import { OrderController } from './order.controller';
 import { PricingModule } from '../products/pricing/pricing.module';
 import { Address } from '../address/address.entity';
 import { OrderCleanupService } from './orderCleanup.service';
+import { Products } from '../products/products.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Order, OrderItem, Cart, CartItem, Address]),
+    TypeOrmModule.forFeature([Order, OrderItem, Cart, CartItem, Address, Products]),
     PricingModule
   ],
   providers: [OrderService, OrderCleanupService],
