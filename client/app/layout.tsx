4e8ea2b8475654370d623fcaecc9a96c1a87847f
NarsheS <shadownightmaresnw@gmail.com>
2026-05-11 17:12:55 -0300

    language change

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-100 min-h-screen flex flex-col`}
       >
