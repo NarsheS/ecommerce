6b510f788bd5af2056f28be377150d57fa3cfc89
NarsheS <shadownightmaresnw@gmail.com>
2026-05-15 20:05:43 -0300

    minor fix on banner.controller, endpoint wasn't public

--- server/src/products/cloudinary/banner/banner.controller.ts
+++ server/src/products/cloudinary/banner/banner.controller.ts
@@ -13,12 +13,14 @@ import { FileInterceptor } from "@nestjs/platform-express"
 import { BannerService } from "./banner.service"
 import { Roles } from "../../../common/roles/roles.decorator";
 import { Role } from "../../../user/user.entity";
+import { Public } from "src/common/decorators/public.decorator";
 
 @Controller("banners")
 export class BannerController {
   constructor(private readonly service: BannerService) {}
 
   @Get()
+  @Public()
   findAll() {
     return this.service.findAll()
   }
